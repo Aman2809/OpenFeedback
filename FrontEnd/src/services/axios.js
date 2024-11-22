f35d8c435defd82fff074b8c79d5434bf7b0ccec
Aman2809 <jhaaman1005@gmail.com>
2024-11-22 15:55:32 +0530

    checking if push problem is resolved in Frontend also or not.

--- FrontEnd/src/services/axios.js
+++ FrontEnd/src/services/axios.js
@@ -5,3 +5,5 @@ export const BASE_URL="http://localhost:9292/api/v1/"
 export const myAxios=axios.create({
     baseURL:BASE_URL
 });
+
+//checking if push problem is resolved in Frontend also or not.
